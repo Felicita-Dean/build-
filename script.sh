f5fb833a4fa6139d969e94d54af5040e748ead58
Felicita Dean <Felicita>
2023-10-23 12:07:38 -0500

    new changes

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 what we need to work on is to get forget our past
 I hang on God all my life for everything 
 I make sure to add more time into my training everyday
+My life is living sacrifice to God
+I like to appreciate God and i have learnt humility in the process 
+I appreciate the advice i get from the people around
