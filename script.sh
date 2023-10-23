48ec344835b7660cb2dd128a587141ed2bcef81c
Felicita Dean <Felicita>
2023-10-23 12:18:08 -0500

    new branch

--- script.sh
+++ script.sh
@@ -4,3 +4,5 @@ I make sure to add more time into my training everyday
 My life is living sacrifice to God
 I like to appreciate God and i have learnt humility in the process 
 I appreciate the advice i get from the people around
+My God has been my defender in all my ways
+I think i am getting better at github by the day 
